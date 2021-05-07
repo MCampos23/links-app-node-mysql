0531c22a5e166eaf145a2f8309bd818a0d0dd720
DESKTOP-FBH0P1R\34605 <camposvmiguelangel@gmail.com>
2021-05-07 12:08:52 +0200

    Create, Read, Update and Delete functionalities

---
+++ src/database.js
@@ -0,0 +1,26 @@
+const mysql = require('mysql')
+const {promisify}= require('util')
+const { database } = require('./keys')
+const pool = mysql.createPool(database)
+
+pool.getConnection((err, connection)=>{
+    if(err){
+        if(err.code==='PROTOCOL_CONECTION_LOST'){
+            console.log('DATABASE CONECTION WAS CLOSED')
+        }
+        if(err.code==='ERR_CON_COUNT_ERROR'){
+            console.log('DATABASE HAS TOO MANY CONNECTIONS')
+        }
+        if(err.code==='ECONREFUSED'){
+            console.log('DATABASE CONECTION WAS REFUSED')
+        }
+    }
+    if(connection) connection.release()
+    console.log('DB is Connnected')
+    return
+})
+
+// Promisify Pool Querys
+pool.query = promisify(pool.query)
+
+module.exports = pool
